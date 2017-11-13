5950c627417d1456ae9e630e2079383562a91eb7
merimem <meriemmemmiche@gmail.com>
2017-11-13 07:35:04 -0800

    Signup users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,7 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
+    
     if @article.save
       flash[:success]= "Article successfully created"
       redirect_to article_path(@article)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'about', to: 'welcome#about'
   
   resources :articles
+
+  get 'signup', to: 'users#new'
+
+  resources :users, except: [:new]
 end
