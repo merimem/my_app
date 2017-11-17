be6591a1e62542928f6b163da31b9b1621167865
merimem <meriemmemmiche@gmail.com>
2017-11-17 13:13:25 -0800

    update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,25 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except:[:index, :show]
   before_action :require_same_user, only:[:edit, :update, :destroy]
+
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @article = Article.new
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:success]= "Article successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -25,17 +38,6 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def create
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.user = current_user
-    if @article.save
-      flash[:success]= "Article successfully created"
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
-  end
 
   def show
     
@@ -55,7 +57,7 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
 
     def require_same_user
